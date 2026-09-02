a8f6bdac3ec58a8d0241c4771edda3dbbfdcda50
ady254 <itsadnanahmad5@gmail.com>
2026-09-02 21:30:51 +0530

    fetch id from database

--- database.py
+++ database.py
@@ -1,14 +1,17 @@
+#In database.py, I'm using SQLAlchemy's declarative base to map a Python class called Product to a PostgreSQL table. 
+# I've defined the columns with their respective data types—like Integers, Strings, and Floats—and set up constraints like a primary key and index on the ID for performance.
 from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+Base = declarative_base()   # create a base class.  Every database model you write will inherit from this Base, allowing SQLAlchemy to track and map your Python classes to actual SQL tables.
 
-class Product(Base):       
+class Product(Base):        # Defines a model named Product that represents a table in your PostgreSQL database.
 
     __tablename__ = 'product'
     id = Column(Integer, primary_key=True, index=True )
     name = Column(String)
-    description = Column(Float)
+    price = Column(Float)
+    description = Column(String)
     quantity = Column (Integer)
 
 
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #In this code, we are building a basic backend REST API using FASTAPI in python, we are managing a mock product inventory store in temporarily in a python list
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from models import Product
-from config import Session, engine
-import database
+from config import Session, engine  # import database configuration from config.py
+import database   # database models (from database.py).
 
 app = FastAPI()
-database.Base.metadata.create_all(bind=engine)
+database.Base.metadata.create_all(bind=engine) # acts as an automated table creation step on startup
 
 @app.get("/")
 def greet():
@@ -20,19 +20,46 @@ products = [
     Product(id=4, name="study table", description= "A long lasting study table", price=399.99, quantity=12),
 ]
 
+def get_db():     # session close
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+#this is a database seeding function. When the application starts up or needs initial test data, init_db opens a database session, iterates through our mock products list, converts each Pydantic model into a dictionary using .model_dump(), and instantiates our SQLAlchemy Product model.
+# Finally, it stages those records using db.add() and persists them to the PostgreSQL database in a single transaction using db.commit().
+
+def init_db():     # written function to commit data into database by mapping the sqlalchemy
+
+    db = Session()
+    count = db.query(database.Product).count
+    if count == 0:
+
+       for product in products:
+            db.add(database.Product(**product.model_dump()))  #product.model_dump(): Converts the Pydantic product model into a standard Python dictionary. (Note: In older versions of Pydantic, this was .dict()). 
+                                                         # ** (Double Asterisk): Unpacks that dictionary into keyword arguments.
+                                                         # database.Product(...): Creates a SQLAlchemy database model instance using those unpacked values. db.add(...): Stages this new product record into the current session queue, telling SQLAlchemy it's ready to be written to the database. 
+        
+       db.commit()
+init_db()
+
 # logic for fetching all the products
 @app.get("/products")       # GET endpoint retrieves and returns the entire list of products stored in the products array
-def get_all_product():
-    db = session()
-    db.query()   
-    return products
+def get_all_products(db: Session = Depends(get_db)):
+    db_products = db.query(database.Product).all()
+
+   
+    return db_products
 # fetch by id
 @app.get("/product/{id}") 
-def get_product_id(id: int):   # here GET endpoint use a dynamic path parameter (id). It iterates through the products list, 
+def get_product_by_id(id: int, db: Session = Depends(get_db)):   # here GET endpoint use a dynamic path parameter (id). It iterates through the products list, 
                               # looks product whose ID matches the requested integer, and return that specific product
-    for product in products:  
-        if product.id == id:  
-            return product
+    db_product = db.query(database.Product).filter(database.Product.id == id).first()
+    if db_product:  
+         return db_product
     return "products not found"  # if no match is found it returns "products not found"
 
 
